8824a5da8088e59e3839ce1a36e89e41df186404
Guillaume Salles <guillaume.salles@me.com>
2017-11-04 14:24:15 -0400

    formatting

--- src/App.js
+++ src/App.js
@@ -7,51 +7,50 @@ import Tile from './Tile/Tile';
 import testImg from './test-img.jpg';
 
 function Tiles(props) {
-  return [...Array(props.nbOfTiles)].map((v, i) => {
-    return <Tile
-      key={i}
-      position={i}
-      total={props.nbOfTiles}
-      source={props.source}
-      sceneMaxSize={props.sceneMaxSize} />;
-  });
+    return [...Array(props.nbOfTiles)].map((v, i) => {
+        return (
+            <Tile
+                key={i}
+                position={i}
+                total={props.nbOfTiles}
+                source={props.source}
+                sceneMaxSize={props.sceneMaxSize}
+            />
+        );
+    });
 }
 
-
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      nbOfTiles: 9,
-      source: testImg,
-      sceneMaxSize: 1024 // this needs to be dynamic
+    constructor(props) {
+        super(props);
+        this.state = {
+            nbOfTiles: 9,
+            source: testImg,
+            sceneMaxSize: 1024 // this needs to be dynamic
+        };
     }
-  }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
 
-        <div className="App-body">
-          <div
-            className="Scene"
-            style={{maxWidth: this.state.sceneMaxSize}}>
-            <div className="Scene-content">
-              <Tiles
-                nbOfTiles={this.state.nbOfTiles}
-                source={this.state.source}
-                sceneMaxSize={this.state.sceneMaxSize}/>
+                <div className="App-body">
+                    <div className="Scene" style={{ maxWidth: this.state.sceneMaxSize }}>
+                        <div className="Scene-content">
+                            <Tiles
+                                nbOfTiles={this.state.nbOfTiles}
+                                source={this.state.source}
+                                sceneMaxSize={this.state.sceneMaxSize}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
